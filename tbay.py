012e69096cb97d85acd687617ed030d4ab56fe00
Ben Altieri <chiefspace@gmail.com>
2016-10-17 06:49:50 +0000

    finally added basic user's bid functionality

--- tbay.py
+++ tbay.py
@@ -4,10 +4,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-#from collections import defaultdict
-
-dictlist = []
-
 
 engine = create_engine('postgresql://ubuntu:thinkful@localhost:5432/tbay')
 Session = sessionmaker(bind=engine)
@@ -21,7 +17,6 @@ from sqlalchemy.orm import relationship
 
 user_bid_item_table = Table('user_bid_item_association', Base.metadata,
     Column(('user_id'), Integer, ForeignKey('users.id')),
-    Column(('bid_id'), Integer, ForeignKey('bids.id')),
     Column(('item_id'), Integer, ForeignKey('items.id'))
 )
 
@@ -43,22 +38,19 @@ class User(Base):
     username = Column(String, nullable=False)
     password = Column(String, nullable=False)
     item = relationship("Item", secondary="user_bid_item_association", backref="users")
-
+    usersbids = relationship("Bid", backref="users")
 
 class Bid(Base):
     __tablename__ = "bids"
 
     id = Column(Integer, primary_key=True)
     price = Column(Float, nullable=False)
-    bid_id = Column(Integer, ForeignKey('items.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)    
 
 
 Base.metadata.create_all(engine)
 
 def main():
-#    item_context = Item(name="Nolan_Ryan_Baseball", description="Nolan Ryan signed baseball")
-
-#    (name="Nolan_Ryan_Baseball", description="Nolan Ryan signed baseball")
 
     baseball = Item(name="Nolan_Ryan_Baseball", description="Nolan Ryan signed baseball")
 
@@ -66,13 +58,19 @@ def main():
     ben.item = [baseball,]
     
     will = User(username="will", password="changeit")
-    
+    baseball.bids = Bid(price=99.00, users=will)
+    baseball.bids = Bid(price=109.00, users=will)
     
     fred = User(username="fred", password="changeit")
+    baseball.bids = Bid(price=110.00, users=fred)
+    baseball.bids = Bid(price=120.00, users=fred)
     
+    baseball.bids = Bid(price=121.00, users=ben)
+    baseball.bids = Bid(price=130.00, users=ben)
+
     session.add_all([ben, will, fred])
     session.commit()
 
-    
+
 if __name__ == "__main__":
     main()
